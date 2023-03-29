62da8c661b3b344db36e5fed767230615d3715f1
hkcwby <alexanderstevenson@me.com>
2023-03-29 02:21:47 -0500

    more mock up work

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,26 @@ function App() {
     <div className="App">
       <div id="background">
         <div id="outer-wheel">
-          <div id="inner-wheel">
+          <div className="location-one">Chatsubo</div>
+          <div className="location-two">Cyberspace</div>
+          <div className="location-three">Gemeinschaft</div>
+          <div className="location-four">Crazy Edo</div>
+          <div className="location-five">Microsofts</div>
+          <div className="location-six">Cheap Hotel</div>
+          <div className="location-seven">Julius Deane</div>
+          <div className="location-eight">Donut World</div>
+          <div className="location-nine">Lawbot</div>
+          <div className="location-ten">database</div>
+          <div className="location-eleven">Shiva</div>
+          <div className="location-twelve">Freeside</div>
+          <div className="location-thirteen">softwarez</div>
+          <div className="location-fourteen">Matrix</div>
+          <div className="location-fifteen">Matt Shaw</div>
+          <div className="location-sixteen">Night City News</div>
+          {/* <div id="inner-wheel">
             <div id="ring-wheel"></div>
-          </div>
+          </div> */}
         </div>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
       </div>
     </div>
   );

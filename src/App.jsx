a87f167d1d2d7a77418c23c6f30439de89c66786
hkcwby <alexanderstevenson@me.com>
2023-04-03 16:58:34 -0500

    working on inner wheel data solutions and mapping

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,115 @@ function App() {
     "Matt Shaw",
     "Night City News",
   ];
-
+  let asanoComputing = "061254";
+  let spacedock = "031770";
+  let spaceColony = "054127";
+  let flatline = "132077";
+  let ai = "71226";
+  let zionCluster = "43267";
+  let marcusGarvey = "45771";
+  let cryptology = "67237";
+  let chibaCity = "3347";
+  let bankOfBerne = "5165";
+  let bankOfZurich = "5165";
   const innerWheel = Object.keys(wheelData.Chatsubo);
+  const layerOneWheel = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    asanoComputing,
+    "",
+    "Space Colony",
+    "",
+    spacedock,
+    "",
+    "",
+    "flatline",
+  ];
+  const layerTwo = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "ZionCluster",
+    "Asano Computing",
+    "",
+    spaceColony,
+    "",
+    "Spacedock",
+    "Marcus Garvey",
+    "",
+    flatline,
+  ];
+  const layerThree = [
+    "AI",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    zionCluster,
+    "",
+    "Cryptology",
+    "",
+    "",
+    "",
+    marcusGarvey,
+    "",
+    "",
+  ];
+  const layerFour = [
+    ai,
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    cryptology,
+    "",
+    "Bank of Berne",
+    "",
+    "",
+    "",
+    "",
+  ];
+  const layerFive = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "Chiba City",
+    "",
+    "",
+    "",
+    bankOfBerne,
+    "",
+    bankOfZurich,
+    "",
+    "",
+  ];
+  const layerSix = [];
+  const layerSeven = [];
+  const layerEight = [];
 
   const [outer, setOuter] = useState(outerWheel);
   const [inner, setInner] = useState(innerWheel);
+  const [layerOne, setLayerOne] = useState(layerOneWheel);
 
   function outerWheelShift(position) {
     const mod = outer.splice(0, position);
@@ -58,6 +162,11 @@ function App() {
                 className={"inner-" + index}
                 onClick={() => innerWheelShift(index)}
               >
+                {item}
+              </div>
+            ))}
+            {layerOne.map((item, index) => (
+              <div key={index} className={"layer-one-" + index}>
                 {item}
               </div>
             ))}

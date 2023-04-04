24f230cf774600ade859e1bf6bf3a3722e73c143
hkcwby <alexanderstevenson@me.com>
2023-04-04 01:27:57 -0500

    completed a rough layout of all the wheel data

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,13 @@ function App() {
   let cryptology = "67237";
   let chibaCity = "3347";
   let bankOfBerne = "5165";
-  let bankOfZurich = "5165";
+  let bankOfZurich = "1053";
+  let fujiElectric = "6124";
+  let holyJoystick = "333";
+  let onoSendai = "725";
+  let hitachiBiotech = "672";
+  let compuJudge = "054";
+
   const innerWheel = Object.keys(wheelData.Chatsubo);
   const layerOneWheel = [
     "",
@@ -51,7 +57,7 @@ function App() {
     "",
     "flatline",
   ];
-  const layerTwo = [
+  const layerTwoWheel = [
     "",
     "",
     "",
@@ -69,7 +75,7 @@ function App() {
     "",
     flatline,
   ];
-  const layerThree = [
+  const layerThreeWheel = [
     "AI",
     "",
     "",
@@ -87,7 +93,7 @@ function App() {
     "",
     "",
   ];
-  const layerFour = [
+  const layerFourWheel = [
     ai,
     "",
     "",
@@ -105,7 +111,7 @@ function App() {
     "",
     "",
   ];
-  const layerFive = [
+  const layerFiveWheel = [
     "",
     "",
     "",
@@ -123,13 +129,71 @@ function App() {
     "",
     "",
   ];
-  const layerSix = [];
-  const layerSeven = [];
-  const layerEight = [];
+  const layerSixWheel = [
+    fujiElectric,
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    chibaCity,
+    "",
+    "",
+    "Holy Joystick",
+    "",
+    "",
+    "Bank of Zurich",
+    "Compu-judge",
+    "",
+  ];
+  const layerSevenWheel = [
+    "Fuji Electric",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "Hitachi Biotech",
+    "",
+    holyJoystick,
+    "",
+    "Ono-Sendai",
+    "",
+    compuJudge,
+    "",
+  ];
+  const layerEightWheel = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    hitachiBiotech,
+    "",
+    "",
+    "",
+    onoSendai,
+    "",
+    "",
+    "",
+  ];
 
   const [outer, setOuter] = useState(outerWheel);
   const [inner, setInner] = useState(innerWheel);
   const [layerOne, setLayerOne] = useState(layerOneWheel);
+  const [layerTwo, setLayerTwo] = useState(layerTwoWheel);
+  const [layerThree, setLayerThree] = useState(layerThreeWheel);
+  const [layerFour, setLayerFour] = useState(layerFourWheel);
+  const [layerFive, setLayerFive] = useState(layerFiveWheel);
+  const [layerSix, setLayerSix] = useState(layerSixWheel);
+  const [layerSeven, setLayerSeven] = useState(layerSevenWheel);
+  const [layerEight, setLayerEight] = useState(layerEightWheel);
 
   function outerWheelShift(position) {
     const mod = outer.splice(0, position);
@@ -167,6 +231,41 @@ function App() {
             ))}
             {layerOne.map((item, index) => (
               <div key={index} className={"layer-one-" + index}>
+                {item}
+              </div>
+            ))}
+            {layerTwo.map((item, index) => (
+              <div key={index} className={"layer-two-" + index}>
+                {item}
+              </div>
+            ))}
+            {layerThree.map((item, index) => (
+              <div key={index} className={"layer-three-" + index}>
+                {item}
+              </div>
+            ))}
+            {layerFour.map((item, index) => (
+              <div key={index} className={"layer-four-" + index}>
+                {item}
+              </div>
+            ))}
+            {layerFive.map((item, index) => (
+              <div key={index} className={"layer-five-" + index}>
+                {item}
+              </div>
+            ))}
+            {layerSix.map((item, index) => (
+              <div key={index} className={"layer-six-" + index}>
+                {item}
+              </div>
+            ))}
+            {layerSeven.map((item, index) => (
+              <div key={index} className={"layer-seven-" + index}>
+                {item}
+              </div>
+            ))}
+            {layerEight.map((item, index) => (
+              <div key={index} className={"layer-eight-" + index}>
                 {item}
               </div>
             ))}

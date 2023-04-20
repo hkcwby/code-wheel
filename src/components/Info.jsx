4c82cea3444ea9998951eebe9a033c5c8e653cae
hkcwby <alexanderstevenson@me.com>
2023-04-19 23:40:55 -0500

    planning out the information panel

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,19 @@
 function Info() {
   return (
     <div id="outer-wheel">
-      <h1>About</h1>
+      <h1>Information</h1>
+      <div>
+        This is a recreation of the Pax Verification Wheel from the 1989 game
+        Neuromancer which is based on the classic Cyberpunk Novel by William
+        Gibson.
+      </div>
+      <div>
+        The game itself is now widely available to play using DosBox on your
+        computer or through a web browser. However in order to play the game
+        properly you require a code wheel which was supplied with the original
+        physical copy of the game. This was a common form of data protection in
+        the 1980s and 1990s for PC gaming.
+      </div>
     </div>
   );
 }

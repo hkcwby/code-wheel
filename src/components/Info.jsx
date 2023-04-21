4c549fc9546b3743dd4327f2d097904ee1a59636
hkcwby <alexanderstevenson@me.com>
2023-04-21 16:06:46 -0500

    completed info panel draft and seperated out its CSS

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,24 +7,63 @@ function Info() {
       <div className="info-body">
         <p className="info-text-top">
           This is a recreation of the Pax Verification Wheel from the 1988 game{" "}
-          <a href="https://en.wikipedia.org/wiki/Neuromancer_(video_game)">
+          <a
+            target="_blank"
+            href="https://en.wikipedia.org/wiki/Neuromancer_(video_game)"
+          >
             Neuromancer
+          </a>
+          , which is based (sadly a little too loosely) on the classic{" "}
+          <a target="_blank" href="https://en.wikipedia.org/wiki/Cyberpunk">
+            Cyberpunk
           </a>{" "}
-          , which is based on the classic Cyberpunk{" "}
-          <a href="https://en.wikipedia.org/wiki/Neuromancer">Novel</a> by
-          William Gibson.
+          <a target="_blank" href="https://en.wikipedia.org/wiki/Neuromancer">
+            Novel
+          </a>{" "}
+          by{" "}
+          <a
+            target="_blank"
+            href="https://en.wikipedia.org/wiki/William_Gibson"
+          >
+            William Gibson
+          </a>
+          .
         </p>
         <p className="info-text-mid">
           The game itself is now widely available to play on modern computers
-          using DosBox or through a web browser. However, in order to play the
-          game properly you require a code wheel which was supplied with the
-          original physical copy of the game. This was a common form of data
-          protection in the 1980s and 1990s for PC gaming.
+          using{" "}
+          <a target="_blank" href="https://www.dosbox.com/">
+            DOSBox
+          </a>{" "}
+          or through a web browser. However, in order to play the game properly
+          you require a code wheel which was supplied with the original physical
+          copy of the game. This was a common form of data protection in the
+          1980s and 1990s for PC gaming.
         </p>
         <p className="info-text-bottom">
           This web application is functional replica of the original code wheel.
-          So you won't need to be a Console Cowboy or crack the ICE to enjoy
-          this game.
+          So you won't need to be a Console Cowboy or crack the{" "}
+          <a
+            target="_blank"
+            href="https://en.wikipedia.org/wiki/Intrusion_Countermeasures_Electronics"
+          >
+            ICE
+          </a>{" "}
+          to enjoy this game; its so easy now any{" "}
+          <a
+            target="_blank"
+            href="https://www.urbandictionary.com/define.php?term=hot%20dogger"
+          >
+            Hot Dogger
+          </a>{" "}
+          or{" "}
+          <a
+            target="_blank"
+            href="https://www.urbandictionary.com/define.php?term=The%20Wilson&page=4"
+          >
+            Wilson
+          </a>{" "}
+          can play.
         </p>
       </div>
     </div>

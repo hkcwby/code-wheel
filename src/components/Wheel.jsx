927da01987cc87b4e87feb18fc0e1081e091aba6
hkcwby <alexanderstevenson@me.com>
2023-05-16 15:42:31 -0500

    standardised the CSS for positioning all the data on the wheel to match the approach used for titles and logo

--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -6,7 +6,7 @@ function Wheel(props) {
         {props.outer.map((item, index) => (
           <div
             key={index}
-            className={`pos-data-${index} data outer`}
+            className={`pos-data-${index} outer`}
             onClick={() => props.outerWheelClick(index)}
           >
             {item}
@@ -17,7 +17,7 @@ function Wheel(props) {
         {props.inner.map((item, index) => (
           <div
             key={index}
-            className={`pos-data-${index} data inner`}
+            className={`pos-data-${index} inner`}
             onClick={() => props.innerWheelClick(index)}
           >
             {item}
@@ -25,119 +25,95 @@ function Wheel(props) {
         ))}
         {props.layers.layerOne.map((item, index) =>
           Number(item) ? (
-            <div
-              key={index}
-              className={`pos-data-${index} data layer-one number`}
-            >
+            <div key={index} className={`pos-data-${index} layer-one number`}>
               {item}
             </div>
           ) : (
-            <div key={index} className={`pos-data-${index} data layer-one`}>
+            <div key={index} className={`pos-data-${index} layer-one`}>
               {item}
             </div>
           )
         )}
         {props.layers.layerTwo.map((item, index) =>
           Number(item) ? (
-            <div
-              key={index}
-              className={`pos-data-${index} data layer-two number`}
-            >
+            <div key={index} className={`pos-data-${index} layer-two number`}>
               {item}
             </div>
           ) : (
-            <div key={index} className={`pos-data-${index} data layer-two`}>
+            <div key={index} className={`pos-data-${index} layer-two`}>
               {item}
             </div>
           )
         )}
         {props.layers.layerThree.map((item, index) =>
           Number(item) ? (
-            <div
-              key={index}
-              className={`pos-data-${index} data layer-three number`}
-            >
+            <div key={index} className={`pos-data-${index} layer-three number`}>
               {item}
             </div>
           ) : (
-            <div key={index} className={`pos-data-${index} data layer-three`}>
+            <div key={index} className={`pos-data-${index} layer-three`}>
               {item}
             </div>
           )
         )}
         {props.layers.layerFour.map((item, index) =>
           Number(item) ? (
-            <div
-              key={index}
-              className={`pos-data-${index} data layer-four number`}
-            >
+            <div key={index} className={`pos-data-${index} layer-four number`}>
               {item}
             </div>
           ) : (
-            <div key={index} className={`pos-data-${index} data layer-four`}>
+            <div key={index} className={`pos-data-${index} layer-four`}>
               {item}
             </div>
           )
         )}
         {props.layers.layerFive.map((item, index) =>
           Number(item) ? (
-            <div
-              key={index}
-              className={`pos-data-${index} data layer-five number`}
-            >
+            <div key={index} className={`pos-data-${index} layer-five number`}>
               {item}
             </div>
           ) : (
-            <div key={index} className={`pos-data-${index} data layer-five`}>
+            <div key={index} className={`pos-data-${index} layer-five`}>
               {item}
             </div>
           )
         )}
         {props.layers.layerSix.map((item, index) =>
           Number(item) ? (
-            <div
-              key={index}
-              className={`pos-data-${index} data layer-six number`}
-            >
+            <div key={index} className={`pos-data-${index} layer-six number`}>
               {item}
             </div>
           ) : (
-            <div key={index} className={`pos-data-${index} data layer-six`}>
+            <div key={index} className={`pos-data-${index} layer-six`}>
               {item}
             </div>
           )
         )}
         {props.layers.layerSeven.map((item, index) =>
           Number(item) ? (
-            <div
-              key={index}
-              className={`pos-data-${index} data layer-seven number`}
-            >
+            <div key={index} className={`pos-data-${index} layer-seven number`}>
               {item}
             </div>
           ) : (
-            <div key={index} className={`pos-data-${index} data layer-seven`}>
+            <div key={index} className={`pos-data-${index} layer-seven`}>
               {item}
             </div>
           )
         )}
         {props.layers.layerEight.map((item, index) =>
           Number(item) ? (
-            <div
-              key={index}
-              className={`pos-data-${index} data layer-eight number`}
-            >
+            <div key={index} className={`pos-data-${index} layer-eight number`}>
               {item}
             </div>
           ) : (
-            <div key={index} className={`pos-data-${index} data layer-eight`}>
+            <div key={index} className={`pos-data-${index} layer-eight`}>
               {item}
             </div>
           )
         )}
         {props.layers.logoTitle.map((item, index) =>
           item ? (
-            <h2 key={index} className={`title-${index} title-pos`}>
+            <h2 key={index} className={`pos-logo-title-${index} title`}>
               NEUROMANCER
             </h2>
           ) : (
@@ -146,7 +122,7 @@ function Wheel(props) {
         )}
         {props.layers.logoTitle.map((item, index) =>
           item ? (
-            <h3 key={index} className={`title-${index} subtitle-pos`}>
+            <h3 key={index} className={`pos-logo-title-${index} subtitle`}>
               PAX Verification Code Wheel
             </h3>
           ) : (
@@ -155,7 +131,11 @@ function Wheel(props) {
         )}
         {props.layers.logoTitle.map((item, index) =>
           item ? (
-            <img src={mainLogo} className={`title-${index} logo`} alt="logo" />
+            <img
+              src={mainLogo}
+              className={`pos-logo-title-${index} logo`}
+              alt="logo"
+            />
           ) : (
             <></>
           )
